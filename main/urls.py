6052e6d2e063c0617c51db0ec41004dab482e3b4
Ashley Wu <ashleywu2004@gmail.com>
2023-08-27 16:36:10 -0700

    Added detail view and create item functionality

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import MovieList
+from .views import MovieList, MovieDetail, MovieCreate
 
 urlpatterns = [
     path('', MovieList.as_view(), name = 'movies'),
+    path('movie/<int:pk>/', MovieDetail.as_view(), name = 'movie'), 
+    path('create/', MovieCreate.as_view(), name = 'create'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
 from .models import Movie
 
 class MovieList(ListView):
     model = Movie
+    context_object_name = 'movies'
+
+class MovieDetail(DetailView):
+    model = Movie
+    context_object_name = 'movie'
+    template_name = 'main/movie.html'
+
+class MovieCreate(CreateView):
+    model = Movie
+    fields = '__all__'
+    success_url = reverse_lazy('movies') # redirects backs to this page 
