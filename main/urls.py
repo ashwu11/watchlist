bb586eea9656be018ac4c7f7406ed632069a6576
Ashley Wu <ashleywu2004@gmail.com>
2023-08-28 02:24:38 -0700

    implement login / logout functionality

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
-from .views import MovieList, MovieDetail, MovieCreate, MovieUpdate, MovieDelete
+from .views import MovieList, MovieDetail, MovieCreate, MovieUpdate, MovieDelete, UserLogin
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', UserLogin.as_view(), name = 'login'),
+    path('logout/', LogoutView.as_view(next_page = 'login'), name = 'logout'),
     path('', MovieList.as_view(), name = 'movies'),
     path('movie/<int:pk>/', MovieDetail.as_view(), name = 'movie'), 
     path('create/', MovieCreate.as_view(), name = 'create'),
--- main/views.py
+++ main/views.py
@@ -3,28 +3,40 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin # have to set login url in settings
+
 from .models import Movie
 
-class MovieList(ListView):
+class UserLogin(LoginView):
+    template_name = "main/login.html"
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('movies')
+
+class MovieList(LoginRequiredMixin, ListView):
     model = Movie
     context_object_name = 'movies'
 
-class MovieDetail(DetailView):
+class MovieDetail(LoginRequiredMixin, DetailView):
     model = Movie
     context_object_name = 'movie'
     template_name = 'main/movie.html'
 
-class MovieCreate(CreateView):
+class MovieCreate(LoginRequiredMixin, CreateView):
     model = Movie
     fields = '__all__'
     success_url = reverse_lazy('movies')
 
-class MovieUpdate(UpdateView):
+class MovieUpdate(LoginRequiredMixin, UpdateView):
     model = Movie
     fields = '__all__'
     success_url = reverse_lazy('movies')
 
-class MovieDelete(DeleteView):
+class MovieDelete(LoginRequiredMixin, DeleteView):
     model = Movie
     context_object_name = 'movie'
     success_url = reverse_lazy('movies')
